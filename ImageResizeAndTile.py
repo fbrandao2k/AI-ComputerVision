bee8060149ba2e8c789d3a06a6d5a570a607254d
Felipe da Silva Brandão <fbrandao2k@gmail.com>
2025-07-20 15:30:20 -0300

    Add files via upload

--- ImageResizeAndTile.py
+++ ImageResizeAndTile.py
@@ -17,9 +17,9 @@ def tile_image_and_save(image, base_name, output_dir, tile_size=416):
 
 # Define paths
 #uploaded_image_path = "D:/AI-ComputerVision/Damages/Crack/PonteJacunda/Drones/01_Zenmuse/DJI_202311301117_002_UgCS-JacundaT60/DJI_20231130112414_0100.JPG"
-uploaded_image_folder = "D:/AI-ComputerVision/Damages/Crack/PonteJacunda/Drones/01_Zenmuse/DJI_202312011453_012_original"
+uploaded_image_folder = "D:/AI-ComputerVision/Damages/Crack/PonteJacunda/Drones/01_Zenmuse/DJI_202312011528_014_original"
 #resized_output_path = "D:/AI-ComputerVision/Damages/Crack/PonteJacunda/Drones/01_Zenmuse/DJI_202311301117_002_UgCS-JacundaT60_resized/DJI_20231130112414_0100_resized.JPG"
-tiled_output_folder = Path("D:/AI-ComputerVision/Damages/Crack/PonteJacunda/Drones/01_Zenmuse/DJI_202312011453_012_tiled416")
+tiled_output_folder = Path("D:/AI-ComputerVision/Damages/Crack/PonteJacunda/Drones/01_Zenmuse/DJI_202312011528_014_tiled416")
 tiled_output_folder.mkdir(parents=True, exist_ok=True)
 
 # Get all image files in the uploaded_image_folder
